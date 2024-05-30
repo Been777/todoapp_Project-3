39ea4b5fb395cefe5d63e725e794b696a765d5ee
Been777 <o7.been@gmail.com>
2024-05-30 22:59:30 +0900

    init Commit

---
+++ src/main/java/com/sparta/todoapp_project3/TodoappProject3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.todoapp_project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todoapp_project3/TodoappProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todoapp_project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
